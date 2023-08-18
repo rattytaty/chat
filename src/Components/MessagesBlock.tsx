deadf5338e36a2d615e0082edde67de242d89934
rattytaty <dmolodovskyi@gmail.com>
2023-08-19 00:04:59 +0100

    messages design

--- src/Components/MessagesBlock.tsx
+++ src/Components/MessagesBlock.tsx
@@ -8,7 +8,7 @@ export const MessagesBlock = () => {
                 <img className="photo" alt="photo" src={"https://www.readersdigest.ca/wp-content/uploads/2017/08/being-a-good-person.jpg"}/>
                 <span>13:37</span>
             </div>
-            <div className="message-blue">
+            <div className="message-left">
                 <p className="message-content">Thandnmanwdjkawdnaknwd!</p>
             </div>
         </div>
@@ -17,13 +17,13 @@ export const MessagesBlock = () => {
                 <img className="photo" alt="photo" src={"https://www.readersdigest.ca/wp-content/uploads/2017/08/being-a-good-person.jpg"}/>
                 <span>13:37</span>
             </div>
-            <div className="message-blue">
+            <div className="message-left">
                 <p className="message-content">Thanawdadwadwawdadadwawdadwaddnmanwdjkawdnaknwd!</p>
             </div>
         </div>
         <div className="message">
 
-            <div className="message-orange">
+            <div className="message-right">
                 <p className="message-content">Thanawdadwadwawdadadwawdadwaddnmanwdjkawdnaknwd!</p>
             </div>
             <div className="messageInfo">
